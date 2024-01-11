99d63e570ba0de7f2ab30328fbfc8d4fe04e70af
samir-27 <sumrasamir27@gmail.com>
2024-01-11 08:44:52 +0530

    add paging

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,35 +1,39 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import { useAppContext } from './context/Context'
-import { API_URL } from '../api'
-import { Navigate, useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useAppContext } from './context/Context';
+import { API_URL } from '../api';
+import { Navigate, useNavigate } from 'react-router-dom';
+
 const Books = () => {
-
-  const [books, setBooks] = useState([])
-
-  const { favorites, AddToFavorites, RemoveFromFavorites } = useAppContext()
-
-  const navigate = useNavigate()
-
-  // console.log(favorites)
+  const [books, setBooks] = useState([]);
+  const [page, setPage] = useState(1);
+  const { favorites, AddToFavorites, RemoveFromFavorites } = useAppContext();
+  const navigate = useNavigate();
 
   const isFavorites = (id) => {
-    const boolean = favorites.some((book) => book.id === id)
-    return boolean
-  }
+    const boolean = favorites.some((book) => book.id === id);
+    return boolean;
+  };
+
+  const fetchBooks = (pageNumber) => {
+    axios.get(`${API_URL}&_page=${pageNumber}`).then((res) => {
+      setBooks(res.data);
+    }).catch((err) => {
+      console.log(err);
+    });
+  };
 
   useEffect(() => {
-    axios.get(API_URL).then(res => {
-      console.log(res.data)
-      setBooks(res.data)
-    }).catch(err => {
-      console.log(err)
-    })
-  }, [])
+    fetchBooks(page);
+  }, [page]);
+
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
+  };
+
   return (
     <div className='bg-gray-200'>
-
-    <div className='container mx-auto flex flex-wrap min-h-80vh justify-center'>
+      <div className='container mx-auto flex flex-wrap min-h-80vh justify-center'>
       {books.map((book) => (
         <div key={book.id} className='bg-white p-4 rounded-md shadow-xl md:m-10 sm:m-2 m-2'>
           <div className='h-16 flex items-center'>
@@ -43,10 +47,25 @@ const Books = () => {
           }
         </div>
       ))}
-    </div>
       </div>
 
-  )
-}
+      <div className='flex justify-center m-4'>
+        <button
+          onClick={() => handlePageChange(page - 1)}
+          disabled={page === 1}
+          className='bg-gray-600 text-white p-2 rounded-lg mr-2'
+        >
+          Previous Page
+        </button>
+        <button
+          onClick={() => handlePageChange(page + 1)}
+          className='bg-gray-600 text-white p-2 rounded-lg'
+        >
+          Next Page
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Books
+export default Books;
